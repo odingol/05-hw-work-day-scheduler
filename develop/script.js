789d73a52350894130aacbcd2e5e134111442e79
Lamor Odingo <l2odingo@gmail.com>
2022-01-28 23:32:11 -0800

    attached past, present, and future to time-block

--- develop/script.js
+++ develop/script.js
@@ -19,225 +19,76 @@ var inputText8 = $('textarea[name="input-box8"]');
 var inputText9 = $('textarea[name="input-box9"]');
 
 
-
-
 // Save Buttons
 
-var saveButton = $('#save1');
-var saveButton2 = $('#save2');
-var saveButton3 = $('#save3');
-var saveButton4 = $('#save4');
-var saveButton5 = $('#save5');
-var saveButton6 = $('#save6');
-var saveButton7 = $('#save7');
-var saveButton8 = $('#save8');
-var saveButton9 = $('#save9');
+var saveButton = $('.saveBtn');
 
 
+// Unique Save Button IDs
+
+var save1 = $('#save1');
 
 
 // Logging & saving text to Textarea
 
-function getInput() {
-        
-        var inputField = {
-            inputText: inputText.val(),
-            inputText2: inputText2.val(),
-            inputText3: inputText3.val(),
-            inputText4: inputText4.val(),
-            inputText5: inputText5.val(),
-            inputText6: inputText6.val(),
-            inputText7: inputText7.val(),
-            inputText8: inputText8.val(),
-            inputText9: inputText9.val(),
-        };
-        
-        localStorage.setItem('text', JSON.stringify(inputField))
 
-};
+function getInput() {
+
+
+    var inputField = {
+    
+        inputText: inputText.val(),
+        inputText2: inputText2.val(),
+        inputText3: inputText3.val(),
+        inputText4: inputText4.val(),
+        inputText5: inputText5.val(),
+        inputText6: inputText6.val(),
+        inputText7: inputText7.val(),
+        inputText8: inputText8.val(),
+        inputText9: inputText9.val(),
+
+    };
+
+    localStorage.setItem('text', JSON.stringify(inputField));
+
+}
 
 
 
 function saveInput() {
 
+
     var inputValue = JSON.parse(localStorage.getItem('text'));
-    
-    if(inputValue !== null) {
+
+    if (inputValue !== null) {
         inputText.text(inputValue.inputText);
         inputText2.text(inputValue.inputText2);
-    }
-    
-    else {
-        return;
-    }
-};
-
-
-
-function saveInput3() {
- 
-    if(inputValue !== null) {
         inputText3.text(inputValue.inputText3);
-    }
-    
-    else {
-        return;
-    }
-};
-
-function saveInput4() {
- 
-    if(inputValue !== null) {
         inputText4.text(inputValue.inputText4);
-    }
-    
-    else {
-        return;
-    }
-};
-
-function saveInput5() {
- 
-    if(inputValue !== null) {
         inputText5.text(inputValue.inputText5);
-    }
-    
-    else {
-        return;
-    }
-};
-
-function saveInput6() {
- 
-    if(inputValue !== null) {
         inputText6.text(inputValue.inputText6);
-    }
-    
-    else {
-        return;
-    }
-};
-
-function saveInput7() {
- 
-    if(inputValue !== null) {
         inputText7.text(inputValue.inputText7);
-    }
-    
-    else {
-        return;
-    }
-};
-
-function saveInput8() {
- 
-    if(inputValue !== null) {
         inputText8.text(inputValue.inputText8);
-    }
-    
-    else {
-        return;
-    }
-};
-
-function saveInput9() {
- 
-    if(inputValue !== null) {
         inputText9.text(inputValue.inputText9);
     }
-    
+
     else {
         return;
     }
 };
 
 
-saveButton.on('click', function(event) {
+
+
+saveButton.on('click', function (event) {
+
     event.preventDefault();
     window.alert('Note was Saved!');
+    console.log(event.target)
     saveInput();
     getInput();
+
 });
-
-// saveButton2.on('click', function(event) {
-//     event.preventDefault();
-//     window.alert('Note was Saved!');
-//     saveInput();
-//     getInput();
-// });
-
-saveButton3.on('click', function(event) {
-    event.preventDefault();
-    window.alert('Note was Saved!');
-    saveInput3();
-    getInput();
-});
-
-saveButton4.on('click', function(event) {
-    event.preventDefault();
-    window.alert('Note was Saved!');
-    saveInput4();
-    getInput();
-});
-
-saveButton5.on('click', function(event) {
-    event.preventDefault();
-    window.alert('Note was Saved!');
-    saveInput5();
-    getInput();
-});
-
-saveButton6.on('click', function(event) {
-    event.preventDefault();
-    window.alert('Note was Saved!');
-    saveInput6();
-    getInput();
-});
-
-saveButton7.on('click', function(event) {
-    event.preventDefault();
-    window.alert('Note was Saved!');
-    saveInput7();
-    getInput();
-});
-
-saveButton8.on('click', function(event) {
-    event.preventDefault();
-    window.alert('Note was Saved!');
-    saveInput8();
-    getInput();
-});
-
-saveButton9.on('click', function(event) {
-    event.preventDefault();
-    window.alert('Note was Saved!');
-    saveInput9();
-    getInput();
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -249,4 +100,63 @@ function maintain() {
 }
 
 maintain();
+
+
+
+
+// Time Block color coded to indicate the past, present, and future 
+
+
+var past = $('.past');
+var present = $('.present');
+var future = $('.future');
+
+var textBox = $('.input-box');
+
+// The current hour 
+
+var presentHour = moment().format('H') //'H' is what displays time as military time as opposed to 'h' where is uses standard time //
+
+
+// Time block Variables
+
+// const time9 = $('#9');
+// const time10 = $('#10');
+
+
+
+
+// Past Function
+
+function pastTime() {
+    for(var pre = presentHour -1; pre >= 9; pre-- ) {
+        if(pre >= 9 && pre <= 17) {
+            document.getElementById(pre).classList.add('past');
+        }
+    }
+}
+
+function currentTime() {
+    if(presentHour >= 9 && presentHour <= 17) {
+        document.getElementById(presentHour).classList.add('present');
+    }
+}
+
+function futureTime() {
+    for(var fut = +presentHour + 1; fut <= 17; fut++) {
+        if(fut <= 17) {
+            document.getElementById(fut).classList.add('future');
+        }
+    }
+}
+
+
+
+pastTime();
+currentTime();
+futureTime();
+
+
+
+
 
